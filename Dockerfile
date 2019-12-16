6ec55838ad3d8ab339deb0972a51b7dec44b3c2f
Maciej Pochroń <m.pochron@stermedia.pl>
2019-12-16 20:59:59 +0100

    Updates

---
+++ Dockerfile
@@ -0,0 +1,81 @@
+FROM ubuntu:16.04
+
+WORKDIR /usr/local/lsws/
+
+RUN apt-get update && apt-get upgrade -y && apt-get install -y wget
+
+RUN wget -O /etc/apt/trusted.gpg.d/lst_debian_repo.gpg http://rpms.litespeedtech.com/debian/lst_debian_repo.gpg
+RUN wget -O /etc/apt/trusted.gpg.d/lst_repo.gpg http://rpms.litespeedtech.com/debian/lst_repo.gpg
+
+RUN echo "deb http://rpms.litespeedtech.com/debian/ xenial main" > /etc/apt/sources.list.d/lst_debian_repo.list
+RUN echo "deb http://rpms.litespeedtech.com/edge/debian/ xenial main" >> /etc/apt/sources.list.d/lst_debian_repo.list
+RUN apt-get update
+RUN apt-get install -y openlitespeed
+RUN apt-get install -y --no-install-recommends lsphp73 lsphp73-common lsphp73-mysql lsphp73-json lsphp73-opcache lsphp73-imap lsphp73-dev lsphp73-curl lsphp73-dbg
+RUN ln -s /usr/local/lsws/lsphp73/bin/php7.3 /usr/bin/php
+
+# Installing wordpress
+RUN wget --no-check-certificate http://wordpress.org/latest.tar.gz
+RUN tar -xzvf latest.tar.gz  >/dev/null 2>&1
+RUN rm latest.tar.gz
+RUN wget -q -r --level=0 -nH --cut-dirs=2 --no-parent https://plugins.svn.wordpress.org/litespeed-cache/trunk/ --reject html -P ./wordpress/wp-content/plugins/litespeed-cache/
+RUN chown -R --reference=./autoupdate ./wordpress
+
+#
+RUN rm -rf /usr/local/lsws/conf/httpd_config.conf /usr/local/lsws/lsphp73/etc/php/7.3/litespeed/php.ini /var/lib/apt/lists/* ./enable_lst_debain_repo.sh /usr/local/lsws/conf/vhosts/Example && apt-get remove --purge -y wget
+
+RUN touch /usr/local/lsws/logs/error.log
+RUN ls -sf /proc/1/fd/1 /usr/local/lsws/logs/error.log
+
+COPY ./httpd_config.conf /usr/local/lsws/conf/
+COPY ./php.ini /usr/local/lsws/lsphp73/etc/php/7.3/litespeed/
+
+COPY ./entrypoint.sh /
+
+VOLUME ["/usr/local/lsws/wordpress"]
+
+EXPOSE 80
+EXPOSE 443
+EXPOSE 443/udp
+EXPOSE 7080
+
+ENV PATH=/usr/local/lsws/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin \
+    DB_NAME='wordpress' \
+    DB_USERNAME='root' \
+    DB_PASSWORD='' \
+    DB_HOST='localhost' \
+    SERVER_EMAIL='info@localhost' \
+    SERVER_LOGIN='admin' \
+    SERVER_PASSWORD='123456' \
+    SSL_COUNTRY='US' \
+    SSL_STATE='New Jersey' \
+    SSL_LOCALITY='Virtual' \
+    SSL_ORG='LiteSpeedCommunity' \
+    SSL_ORGUNIT='Testing' \
+    SSL_HOSTNAME='webadmin' \
+    SSL_EMAIL='.' \
+    WORDPRESS_TABLE_PREFIX='wp_' \
+    WORDPRESS_DEBUG="0" 
+
+ENTRYPOINT ["/entrypoint.sh"]
+
+
+# [supervisord]
+# nodaemon=true
+
+# [program:startup]
+# priority=1
+# command=/root/credentialize_and_run.sh
+# stdout_logfile=/var/log/supervisor/%(program_name)s.log
+# stderr_logfile=/var/log/supervisor/%(program_name)s.log
+# autorestart=false
+# startsecs=0
+
+# [program:nginx]
+# priority=10
+# command=nginx -g "daemon off;"
+# stdout_logfile=/var/log/supervisor/nginx.log
+# stderr_logfile=/var/log/supervisor/nginx.log
+# autorestart=true
+
+# CMD ["/usr/bin/supervisord", "-n"]
---
+++ entrypoint.sh
@@ -0,0 +1,261 @@
+#!/bin/bash
+
+SERVER_ROOT=/usr/local/lsws
+WORDPRESSPATH=$SERVER_ROOT/wordpress
+WPPORT=80
+SSLWPPORT=443
+
+CSR=example.csr
+KEY=example.key
+CERT=example.crt
+
+SITEDOMAIN=*
+
+sed_escape_lhs() {
+    echo "$@" | sed -e 's/[]\/$*.^|[]/\\&/g'
+}
+
+sed_escape_rhs() {
+    echo "$@" | sed -e 's/[\/&]/\\&/g'
+}
+
+php_escape() {
+    php -r 'var_export(('$2') $argv[1]);' -- "$1"
+}
+
+function set_config() 
+{
+    key="$1"
+    value="$2"
+    php_escaped_value="$(php -r 'var_export($argv[1]);' "$value")"
+    sed_escaped_value="$(echo "$php_escaped_value" | sed 's/[\/&]/\\&/g')"
+
+    sed -ri "s/((['\"])$key\2\s*,\s*)(['\"]).*\3/\1$sed_escaped_value/" "$WORDPRESSPATH/wp-config.php"
+
+    # key="$1"
+    # value="$2"
+    # var_type="${3:-string}"
+    # start="(['\"])$(sed_escape_lhs "$key")\2\s*,"
+    # end="\);"
+    # if [ "${key:0:1}" = '$' ]; then
+    #   start="^(\s*)$(sed_escape_lhs "$key")\s*="
+    #   end=";"
+    # fi
+
+    # echo "$key $value $start $end"
+    # echo "sed s/($start\s*).*($end)$/\1$(sed_escape_rhs "$(php_escape "$value" "$var_type")")\3/"
+    
+    # sed -ri -e "s/($start\s*).*($end)$/\1$(sed_escape_rhs "$(php_escape "$value" "$var_type")")\3/" "$WORDPRESSPATH/wp-config.php"
+}
+
+
+function setup_wordpress
+{
+    if [ -e "$WORDPRESSPATH/wp-config.php" ] ; then
+        echo "WordPress Config is existing."
+    else
+        if [ -e "$WORDPRESSPATH/wp-config-sample.php" ] ; then
+            cp "$WORDPRESSPATH/wp-config-sample.php" "$WORDPRESSPATH/wp-config.php"
+            # sed -e "s/database_name_here/$DB_NAME/" -e "s/username_here/$DB_USERNAME/" -e "s/password_here/$DB_PASSWORD/" "$WORDPRESSPATH/wp-config-sample.php" > "$WORDPRESSPATH/wp-config.php"
+            if [ -e "$WORDPRESSPATH/wp-config.php" ] ; then
+                chown  -R --reference="$WORDPRESSPATH/wp-config-sample.php"   "$WORDPRESSPATH/wp-config.php"
+                echo "Finished setting up WordPress."
+            else
+                echo "WordPress setup failed. You may not have sufficient privileges to access $WORDPRESSPATH/wp-config.php."
+            fi
+        else
+            echo "WordPress setup failed. File $WORDPRESSPATH/wp-config-sample.php does not exist."
+        fi
+    fi
+
+    UNIQUES=(
+        AUTH_KEY
+        SECURE_AUTH_KEY
+        LOGGED_IN_KEY
+        NONCE_KEY
+        AUTH_SALT
+        SECURE_AUTH_SALT
+        LOGGED_IN_SALT
+        NONCE_SALT
+    )
+
+    for unique in "${UNIQUES[@]}"; do
+        uniqVar="\$WORDPRESS_$unique"
+        # file_env "$uniqVar"
+        if [ "${!uniqVar}" ]; then
+            set_config "$unique" "${!uniqVar}"
+        else
+            # if not specified, let's generate a random value
+            # define( 'DB_NAME', 'database_name_here' );
+			    	currentVal="$(sed -rn "s/define\(\s*['\"]$unique['\"]\s*,\s*['\"](.*)['\"]\s*\);.*$/\1/p" $WORDPRESSPATH/wp-config.php)"
+
+            if [ "$currentVal" = "put your unique phrase here" ]; then
+                set_config "$unique" "$(head -c1m /dev/urandom | sha1sum | cut -d' ' -f1)"
+            fi
+        fi
+    done
+
+    set_config 'DB_HOST' "$DB_HOST"
+    set_config 'DB_USER' "$DB_USERNAME"
+    set_config 'DB_PASSWORD' "$DB_PASSWORD"
+    set_config 'DB_NAME' "$DB_NAME"
+    set_config 'WORDPRESS_TABLE_PREFIX' "$WORDPRESS_TABLE_PREFIX"
+    set_config 'WORDPRESS_DEBUG' "$WORDPRESS_DEBUG"
+    
+}
+
+
+function set_ols_password
+{
+    #setup password
+    ENCRYPT_PASS=`"$SERVER_ROOT/admin/fcgi-bin/admin_php" -q "$SERVER_ROOT/admin/misc/htpasswd.php" $SERVER_PASSWORD`
+    if [ $? = 0 ] ; then
+        echo "$SERVER_LOGIN:$ENCRYPT_PASS" > "$SERVER_ROOT/admin/conf/htpasswd"
+        if [ $? = 0 ] ; then
+            echo "Finished setting OpenLiteSpeed WebAdmin password to $SERVER_PASSWORD."
+            echo "Finished updating server configuration."
+
+        else
+            echo "OpenLiteSpeed WebAdmin password not changed."
+        fi
+    fi
+
+}
+
+function gen_selfsigned_cert
+{
+
+
+# Create the certificate signing request
+    openssl req -new -passin pass:password -passout pass:password -out $CSR <<EOF
+${SSL_COUNTRY}
+${SSL_STATE}
+${SSL_LOCALITY}
+${SSL_ORG}
+${SSL_ORGUNIT}
+${SSL_HOSTNAME}
+${SSL_EMAIL}
+.
+.
+EOF
+    echo ""
+
+    [ -f ${CSR} ] && openssl req -text -noout -in ${CSR}
+    echo ""
+
+# Create the Key
+    openssl rsa -in privkey.pem -passin pass:password -passout pass:password -out ${KEY}
+# Create the Certificate
+    openssl x509 -in ${CSR} -out ${CERT} -req -signkey ${KEY} -days 1000
+
+    mv ${KEY}   $SERVER_ROOT/conf/$KEY
+    mv ${CERT}  $SERVER_ROOT/conf/$CERT
+    chmod 0600 $SERVER_ROOT/conf/$KEY
+    chmod 0600 $SERVER_ROOT/conf/$CERT
+}
+
+
+function config_server_wp
+{
+    if [ -e "$SERVER_ROOT/conf/httpd_config.conf" ] ; then
+
+        cat $SERVER_ROOT/conf/httpd_config.conf | grep "virtualhost wordpress" >/dev/null
+        if [ $? != 0 ] ; then
+            sed -i -e "s/adminEmails/adminEmails $SERVER_EMAIL\n#adminEmails/" "$SERVER_ROOT/conf/httpd_config.conf"
+
+            VHOSTCONF=$SERVER_ROOT/conf/vhosts/wordpress/vhconf.conf
+
+            cat >> $SERVER_ROOT/conf/httpd_config.conf <<END
+
+virtualhost wordpress {
+  vhRoot                  $WORDPRESSPATH
+  configFile              $VHOSTCONF
+  allowSymbolLink         1
+  enableScript            1
+  restrained              0
+  setUIDMode              2
+}
+
+listener wordpress {
+  address                 *:$WPPORT
+  secure                  0
+  map                     wordpress $SITEDOMAIN
+}
+
+listener wordpressssl {
+  address                 *:$SSLWPPORT
+  secure                  1
+  map                     wordpress $SITEDOMAIN
+  keyFile                 $SERVER_ROOT/conf/$KEY
+  certFile                $SERVER_ROOT/conf/$CERT
+}
+
+
+END
+            mkdir -p $SERVER_ROOT/conf/vhosts/wordpress/
+            cat > $VHOSTCONF <<END
+      
+docRoot                   \$VH_ROOT/
+index  {
+  useServer               0
+  indexFiles              index.php
+}
+
+context / {
+  type                    NULL
+  location                \$VH_ROOT
+  allowBrowse             1
+  indexFiles              index.php
+
+  rewrite  {
+    enable                1
+    inherit               1
+    rewriteFile           $WORDPRESSPATH/.htaccess
+
+  }
+}
+
+END
+            chown -R lsadm:lsadm $SERVER_ROOT/conf/
+        fi
+
+
+    else
+        echoR "$SERVER_ROOT/conf/httpd_config.conf is missing. It appears that something went wrong during OpenLiteSpeed installation."
+        ALLERRORS=1
+    fi
+}
+
+function activate_cache
+{
+    cat > $WORDPRESSPATH/activate_cache.php <<END
+<?php
+include '$WORDPRESSPATH/wp-load.php';
+include_once '$WORDPRESSPATH/wp-admin/includes/plugin.php';
+include_once '$WORDPRESSPATH/wp-admin/includes/file.php';
+define('WP_ADMIN', true);
+activate_plugin('litespeed-cache/litespeed-cache.php', '', false, false);
+
+END
+    ls -all $SERVER_ROOT/fcgi-bin/
+    $SERVER_ROOT/lsphp73/bin/php7.3 $WORDPRESSPATH/activate_cache.php
+    rm $WORDPRESSPATH/activate_cache.php
+}
+
+setup_wordpress
+set_ols_password
+gen_selfsigned_cert
+config_server_wp
+activate_cache
+
+echo "Starting openlitespeed..."
+
+/usr/local/lsws/bin/lswsctrl start
+# openlitespeed -n
+
+
+cat "$WORDPRESSPATH/wp-config.php"
+
+echo "Tailling. ..."
+
+tail -f /dev/null
